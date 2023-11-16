4f5dc43d073cd4aad28fa409ec46c02c601b8541
Kimskogfelter <>
2023-11-16 13:35:00 +0000

    add onclick event on start and stop button on the game area html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
 let timer;
-let memoryCards = []
+let memoryCards = [<img src="assets/images/toad.png"></img>, <img src="assets/images/toad.png"></img>, <img src="assets/images/super-mario.png"></img>, <img src="assets/images/super-mario.png"></img>, <img src="assets/images/super-mario.png"></img>,
+    <img src = "assets/images/scared-boo.png">, <img src = "assets/images/scared-boo.png"></img>, <img src = "assets/images/princess-peach.png"></img>, <img src ="assets/images/princess-peach.png"></img>
+        <img src="assets/images/bowser.png"></img>, <img src="assets/images/bowser.png"></img>, <img src="assets/images/goomba.png"></img>, <img src="assets/images/goomba.png"></img></img>];
+
+console.log(memoryCards);
