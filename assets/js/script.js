d7c28b99e684d93a2b653777a4cc5dcadc0ce1a4
Kimskogfelter <>
2023-11-16 15:30:24 +0000

    added var for startbutton, stopbutton, timer, moves and memoryCardImages. added eventlistener for when clicking on the start and pause button to make the timer start and stop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-let timer;
-let memoryCards = [<img src="assets/images/toad.png"></img>, <img src="assets/images/toad.png"></img>, <img src="assets/images/super-mario.png"></img>, <img src="assets/images/super-mario.png"></img>, <img src="assets/images/super-mario.png"></img>,
-    <img src = "assets/images/scared-boo.png">, <img src = "assets/images/scared-boo.png"></img>, <img src = "assets/images/princess-peach.png"></img>, <img src ="assets/images/princess-peach.png"></img>
-        <img src="assets/images/bowser.png"></img>, <img src="assets/images/bowser.png"></img>, <img src="assets/images/goomba.png"></img>, <img src="assets/images/goomba.png"></img></img>];
+let startButton = document.getElementById('startbutton');
+let pauseButton = document.getElementById('stopbutton');
+let timer = document.getElementById('timer');
+let moves = document.getElementById('moves');
 
-console.log(memoryCards);
+
+let memoryCardImages = [<img src="assets/images/toad.png"></img>, <img src="assets/images/toad.png"></img>, <img src="assets/images/super-mario.png"></img>, <img src="assets/images/super-mario.png"></img>, <img src="assets/images/super-mario.png"></img>,
+<img src="assets/images/scared-boo.png">, <img src="assets/images/scared-boo.png"></img>, <img src="assets/images/princess-peach.png"></img>, <img src="assets/images/princess-peach.png"></img>
+    <img src="assets/images/bowser.png"></img>, <img src="assets/images/bowser.png"></img>, <img src="assets/images/goomba.png"></img>, <img src="assets/images/goomba.png"></img></img>];
+
+
+/* starting the timer when user press "play game" button */
+start.addEventListener('click', function startGame() {
+    timer = setInterval(function () {
+        console.log('!');
+    }, 1000);
+});
+/* makes the timer stop when the user press the "pause game" button */
+stop.addEventListener('click', function pauseGame() {
+    clearInterval(timer);
+});
